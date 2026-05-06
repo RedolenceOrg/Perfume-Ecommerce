4d373a7099b7b34bef65cae07b74ef9d500474a7
RationallyPi <anwesh.atreya@gmail.com>
2026-05-06 11:51:02 +0545

    fixed production key

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nt#a7f4m)n7lsyw#5(q6tp61bgj2=#$n$4b1qw2zl26_z)l=h5'
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
