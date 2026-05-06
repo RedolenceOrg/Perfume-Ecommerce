8f7b17b15015de5dc33d5f153a59e8bcc7e60e93
RationallyPi <anwesh.atreya@gmail.com>
2026-05-06 13:16:05 +0545

    fixed deprecatd image domains

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['localhost:3000'],
   images: {
-    domains: ['127.0.0.1'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+      },
+      {
+        protocol: 'https',
+        hostname: 'perfume-ecommerce-production.up.railway.app',
+      },
+    ],
   },
 };
 
