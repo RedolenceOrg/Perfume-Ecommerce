1cd594b6d5699785f4e7876853fa7b396c7455c0
RationallyPi <anwesh.atreya@gmail.com>
2026-05-06 19:49:52 +0545

    fixed environment call

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -55,8 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
-
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS')
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000').split(',')
 ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
