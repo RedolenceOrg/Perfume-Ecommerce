dc43e1a1e913e9b5c2bc70f26fcbaf84520356f4
RationallyPi <anwesh.atreya@gmail.com>
2026-06-02 17:37:04 +0545

    updated db setting check

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -127,16 +127,16 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASE_URL = config('DATABASE URL',default =False)
+DATABASE_URL = config('DATABASE_URL', default=None)
+
 if DATABASE_URL:
-    
     DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
             ssl_require=True
         )
     }
-
 else:
     DATABASES = {
         'default': {
